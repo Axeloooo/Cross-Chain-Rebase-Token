46bb81380f3d70b3863f2355d315516441b70ced
Axel Sanchez <axelshz@gmail.com>
2026-03-04 20:01:43 -0700

    feat: Add OpenZeppelin contracts as submodule and implement RebaseToken contract with interest rate management

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -1,4 +1,101 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-contract RebaseToken {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title RebaseToken
+ * @author Axel Sanchez
+ * @notice This is a cross-chain rebase token that incentivizes users to deposit into a vault and gain interest in rewards.
+ * @notice The interest rate in the smart contract can only decrease.
+ * @notice Each user will have their own interest rate that is the global rate at time of depositing.
+ */
+contract RebaseToken is ERC20 {
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
+
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 private s_interestRate = 5e10;
+    mapping(address => uint256) private s_userInterestRates;
+    mapping(address => uint256) private s_userLastUpdatedTimestamp;
+
+    event InterestRateUpdated(uint256 newInterestRate);
+
+    constructor() ERC20("Rebase Token", "RBT") {}
+
+    /**
+     * @notice Set the interest rate in the smart contract
+     * @param _newInterestrate The new interest rate to set
+     */
+    function setInterestRate(uint256 _newInterestrate) external {
+        if (_newInterestrate < s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestrate);
+        }
+        s_interestRate = _newInterestrate;
+        emit InterestRateUpdated(_newInterestrate);
+    }
+
+    /**
+     * @notice Mint the user tokens when they deposit into the vault
+     * @param _to The address to mint tokens to
+     * @param _amount The amount of tokens to mint
+     */
+    function mint(address _to, uint256 _amount) external {
+        _mintAccruedInterest(_to);
+        s_userInterestRates[_to] = s_interestRate;
+        _mint(_to, _amount);
+    }
+
+    /**
+     * @notice Burn the user tokens when they withdraw from the vault
+     * @param _from The address to burn tokens from
+     * @param _amount The amount of tokens to burn
+     */
+    function burn(address _from, uint256 _amount) external {
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_from);
+        }
+        _mintAccruedInterest(_from);
+        _burn(_from, _amount);
+    }
+
+    /**
+     * @notice Calculate the balance for the user including any interest accumulated since the last update (principle balance) + some interest that has accrued
+     * @param _user The user to calculate the balance for
+     * @return The balance for the user including any interest accumulated since the last update
+     */
+    function balanceOf(address _user) public view override returns (uint256) {
+        return super.balanceOf(_user) + _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
+    }
+
+    /**
+     * @notice Calculate the interest accumulated for a user since the last update
+     * @param _user The user to calculate the interest accumulated for
+     * @return The interest accumulated for the user since the last update
+     */
+    function _calculateUserAccumulatedInterestSinceLastUpdate(address _user) internal view returns (uint256) {
+        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
+        uint256 linearInterest = PRECISION_FACTOR + (s_userInterestRates[_user] * timeElapsed);
+        return linearInterest;
+    }
+
+    /**
+     * @notice Mint the accrued interest to the user since the last time they interacted with the protocol (e.g. burn, mint, transfer)
+     * @param _user The user to mint accrued interest to
+     */
+    function _mintAccruedInterest(address _user) internal {
+        uint256 previousPrincipleBalance = super.balanceOf(_user);
+        uint256 currentBalance = balanceOf(_user);
+        uint256 balanceIncrease = currentBalance - previousPrincipleBalance;
+        s_userLastUpdatedTimestamp[_user] = block.timestamp;
+        _mint(_user, balanceIncrease);
+    }
+
+    /**
+     * @notice Get the interest rate for a user
+     * @param _user The user to get the interest rate for
+     * @return The interest rate for the user
+     */
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return s_userInterestRates[_user];
+    }
+}
