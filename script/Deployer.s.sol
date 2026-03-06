289d865d75a84831396e938e47c38eea4c132fc4
Axel Sanchez <axelshz@gmail.com>
2026-03-06 15:08:57 -0700

    feat: Implement ConfigurePoolScript and enhance TokenAndPoolDeployer with CCIP integration

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -6,8 +6,31 @@ import {Vault} from "../src/Vault.sol";
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
+import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {Register} from "@chainlink-local/src/ccip/Register.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {RegistryModuleOwnerCustom} from "@ccip/contracts/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import {TokenAdminRegistry} from "@ccip/contracts/tokenAdminRegistry/TokenAdminRegistry.sol";
 
-contract TokenAndPoolDeployer is Script {}
+contract TokenAndPoolDeployer is Script {
+    function run() public returns (RebaseToken, RebaseTokenPool) {
+        CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        Register.NetworkDetails memory networkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+        vm.startBroadcast();
+        RebaseToken rebaseToken = new RebaseToken();
+        RebaseTokenPool rebaseTokenPool = new RebaseTokenPool(
+            IERC20(address(rebaseToken)), new address[](0), networkDetails.rmnProxyAddress, networkDetails.routerAddress
+        );
+        rebaseToken.grantMintAndBurnRole(address(rebaseTokenPool));
+        RegistryModuleOwnerCustom(networkDetails.registryModuleOwnerCustomAddress)
+            .registerAdminViaOwner(address(rebaseToken));
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(rebaseToken));
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress)
+            .setPool(address(rebaseToken), address(rebaseTokenPool));
+        vm.stopBroadcast();
+        return (rebaseToken, rebaseTokenPool);
+    }
+}
 
 contract VaultDeployer is Script {
     function run(address _rebaseToken) public returns (Vault) {
